17a5d8444e575fc56f1802aaba10a03f6770f796
Eddie <70070543+cake287@users.noreply.github.com>
2024-10-22 17:29:39 +0100

    Add files via upload

--- imageRegression.py
+++ imageRegression.py
@@ -6,33 +6,35 @@ from torchvision.utils import save_image
 import matplotlib.pyplot as plt
 import numpy as np
 import math
+import os
 
-inputImage = R"data\imageRegression\melon.jpg" #"monkey.png"
-outputImage = R"test\_melonPred" # "_monkeyPred" # without file extension
+# input and output paths. do not include file extension on output path
+images = [(R"melon.jpg", R"output\melon"), (R"monkey.png", R"output\monkey")]
+
+def DataFromPath(path):
+    image = io.read_image(path, io.ImageReadMode.RGB)
+    image = image.permute(1, 2, 0)
+
+    res = image.shape[0]
+
+    spaced_coords = np.linspace(0, 1, res + 1)[:-1]
+    norm_coords = np.stack(np.meshgrid(spaced_coords, spaced_coords), axis=-1)
+    x_train = torch.from_numpy(norm_coords.reshape(-1, 2)).to(torch.float32) # a list of normalised coordinates (one for each pixel) 
+
+    y_train = image.reshape(-1, 3).float() / 255
+
+    train = TensorDataset(x_train, y_train)
+    return (DataLoader(train, batch_size=1000, shuffle=True), x_train, res)
 
 
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(f"Using device {device}")
 
-image = io.read_image(inputImage, io.ImageReadMode.RGB)
-image = image.permute(1, 2, 0)
-
-RES = image.shape[0]
-
-spaced_coords = np.linspace(0, 1, RES + 1)[:-1]
-norm_coords = np.stack(np.meshgrid(spaced_coords, spaced_coords), axis=-1)
-x_train = torch.from_numpy(norm_coords.reshape(-1, 2)).to(torch.float32) # a list of normalised coordinates (one for each pixel) 
-
-y_train = image.reshape(-1, 3).float() / 255
-
-train = TensorDataset(x_train, y_train)
-train_dataloader = DataLoader(train, batch_size=1000, shuffle=True)
-
-
 class NoEncNetwork(nn.Module):
     def __init__(self):
         super().__init__()
+        self.name = "no-enc"
         self.flatten = nn.Flatten()
         self.linear_relu_stack = nn.Sequential(
             nn.Linear(2, 256),
@@ -59,6 +61,7 @@ class GaussEncNetwork(nn.Module):
     def __init__(self):
         super().__init__()
         self.flatten = nn.Flatten()
+        self.name = "gauss-enc"
         self.linear_relu_stack = nn.Sequential(
             nn.Linear(512, 256),
             nn.ReLU(),
@@ -93,6 +96,7 @@ class PosEncNetwork(nn.Module):
     def __init__(self):
         super().__init__()
         self.flatten = nn.Flatten()
+        self.name = "pos-enc"
         self.encScale = 6
         self.linear_relu_stack = nn.Sequential(
             nn.Linear(4*self.encScale, 256),
@@ -130,8 +134,6 @@ def train(dataloader, model, loss_fn, optimizer):
     for batch, (X, y) in enumerate(dataloader):
         X, y = X.to(device), y.to(device)
 
-        
-
         pred = model(X)
         loss = loss_fn(pred, y) 
 
@@ -145,16 +147,18 @@ def train(dataloader, model, loss_fn, optimizer):
             print(f"loss: {loss:>5f} [{current:>5d}/{size:>5d}]")
 
 
-def save_pred_image(model, path):
+def save_pred_image(model, res, x_train, path, name):
     model.eval()
     with torch.no_grad():
         x_train2 = x_train.to(device)
 
         cols = model(x_train2)
         cols = torch.clamp(cols, min=0, max=1)
-        img = cols.reshape(RES, RES, 3)
+        img = cols.reshape(res, res, 3)
         
-        save_image(img.permute(2, 0, 1), path)
+        if not os.path.exists(path):
+            os.makedirs(path)
+        save_image(img.permute(2, 0, 1), f"{path}{name}")
         print("saved predicted image")
 
         # plt.axis('off')
@@ -163,17 +167,16 @@ def save_pred_image(model, path):
         # plt.show()
 
 
-id = 1
-for model in [PosEncNetwork().to(device)]: #, NoEncNetwork().to(device)]:
-    loss_fn = nn.MSELoss()
-    optimizer = torch.optim.SGD(model.parameters(), lr = 1e-3, momentum=0.9)
+for (inputPath, outputPath) in images:
+    (train_dataloader, x_train, res) = DataFromPath(inputPath)
+    for model in [GaussEncNetwork().to(device), PosEncNetwork().to(device), NoEncNetwork().to(device)]:
+        loss_fn = nn.MSELoss()
+        optimizer = torch.optim.SGD(model.parameters(), lr = 1e-3, momentum=0.9)
 
-    epochs = 1000
-    for t in range(epochs):
-        if t % 10 == 9:
-            print(f"Epoch {t}")
-            save_pred_image(model, f"{outputImage}{id}.png")
-        train(train_dataloader, model, loss_fn, optimizer)
-
-    id += 1
+        epochs = 20
+        for t in range(epochs):
+            if t % 10 == 9:
+                print(f"Epoch {t}")
+                save_pred_image(model, res, x_train, f"{outputPath}/{model.name}/", f"{t // 1000}k.png")
+            train(train_dataloader, model, loss_fn, optimizer)
 
